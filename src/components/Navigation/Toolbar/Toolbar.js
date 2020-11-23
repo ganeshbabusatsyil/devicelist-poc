705384c85d3ce0b74fe094a1b5ac8e38d9623f88
ganeshbabu85 <seepana.ganesh@gmail.com>
2020-11-23 17:56:27 +0530

    css and other changes added

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -5,7 +5,7 @@ import classes from './Toolbar.module.css';
 const Toolbar = (props) => {
     return (
         <header className={classes.Toolbar}>
-            <div>MENU</div>
+            <div>LOGO</div>
             <nav>
                 <NavigationItems />
             </nav>
